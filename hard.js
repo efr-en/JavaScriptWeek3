6818e869d65fe10ad34f3b50b7e66e1695e05d8d
efr-en <efrenlaris@gmail.com>
2024-11-16 15:51:15 -0500

    added all challenges to individual files with the requirements commmented out

---
+++ hard.js
@@ -0,0 +1,10 @@
+// Inside a closure, create an object called PII (Personally Identifiable Information)
+// that cannot be accessed directly. The object should have at least two properties: 
+// name and SSN. Only the name property should be accessible, and it should be called 
+// through a public function. The SSN property should not be accessible at all. Creating
+// private objects and private properties helps you control who has access to what data 
+// and helps you prevent people who shouldn't see important info like social security 
+// numbers from getting access to the data. You can use 'getName' or 
+// other get methods to access data that people might need.
+//  For example, people addressing a package or email may need a customer's name,
+//   but they definitely shouldn't have access to their SSN.
