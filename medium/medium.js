f054a9e935448173e67333c041d4596fb1cbe2be
efr-en <efrenlaris@gmail.com>
2024-11-16 18:30:55 -0500

    medium done

--- medium/medium.js
+++ medium/medium.js
@@ -3,3 +3,17 @@
 // return an interpolated string like Each person gets 4.00 slices of pizza; 
 // from our 8-slice pizza x being a decimal in case there is no way to split 
 // the pizza evenly.
+// x = slices in the pizza
+// y = number of people
+
+function calculatePizzaSlices(x, y) {
+    // let x = slices;
+    // let y = people;
+    const slicesPerPerson = (x / y).toFixed(2); //toFixed returns the decimal points
+    return `Each person gets ${slicesPerPerson} of pizza; from our ${x} slices of pizza`
+}
+
+console.log(calculatePizzaSlices(5, 2));
+console.log(calculatePizzaSlices(10, 2));
+console.log(calculatePizzaSlices(5, 7));
+console.log(calculatePizzaSlices(88, 103));
